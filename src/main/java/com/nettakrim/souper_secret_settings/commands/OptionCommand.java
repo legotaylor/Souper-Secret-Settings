9aa7c3add5e80c84d70b1e54a9f1bbab06b00253
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-03-15 17:44:09 +0000

    eaten item commands now have feedback in chat

--- src/main/java/com/nettakrim/souper_secret_settings/commands/OptionCommand.java
+++ src/main/java/com/nettakrim/souper_secret_settings/commands/OptionCommand.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.ItemStackArgument;
 import net.minecraft.command.argument.ItemStackArgumentType;
+import net.minecraft.item.ItemStack;
 
 public class OptionCommand {
     public OptionCommand() {
@@ -25,6 +26,7 @@ public class OptionCommand {
                         ClientCommandManager.argument("item", ItemStackArgumentType.itemStack(commandRegistryAccess))
                                 .executes(context -> setRandomItem(ItemStackArgumentType.getItemStackArgument(context, "item")))
                 )
+                .executes(context -> queryRandomItem())
                 .build();
         commandNode.addChild(randomNode);
 
@@ -34,20 +36,40 @@ public class OptionCommand {
                         ClientCommandManager.argument("item", ItemStackArgumentType.itemStack(commandRegistryAccess))
                                 .executes(context -> setClearItem(ItemStackArgumentType.getItemStackArgument(context, "item")))
                 )
+                .executes(context -> queryClearItem())
                 .build();
         commandNode.addChild(clearNode);
     }
 
     int setRandomItem(ItemStackArgument itemStack) throws CommandSyntaxException {
         SouperSecretSettingsClient.soupData.config.randomItem = itemStack.createStack(1, false);
+        sayItem("option.random_set", SouperSecretSettingsClient.soupData.config.randomItem);
         SouperSecretSettingsClient.soupData.saveConfig();
+        return 1;
+    }
+
+    int queryRandomItem() {
+        sayItem("option.random_query", SouperSecretSettingsClient.soupData.config.randomItem);
         return 1;
     }
 
     int setClearItem(ItemStackArgument itemStack) throws CommandSyntaxException {
         SouperSecretSettingsClient.soupData.config.clearItem = itemStack.createStack(1, false);
+        sayItem("option.clear_set", SouperSecretSettingsClient.soupData.config.clearItem);
         SouperSecretSettingsClient.soupData.saveConfig();
         return 1;
     }
 
+    int queryClearItem() {
+        sayItem("option.clear_query", SouperSecretSettingsClient.soupData.config.clearItem);
+        return 1;
+    }
+
+    void sayItem(String key, ItemStack itemStack) {
+        String s = itemStack.getItem().toString();
+        if (!itemStack.getComponentChanges().isEmpty()) {
+            s += " "+itemStack.getComponentChanges().toString();
+        }
+        SouperSecretSettingsClient.say(key, s);
+    }
 }
