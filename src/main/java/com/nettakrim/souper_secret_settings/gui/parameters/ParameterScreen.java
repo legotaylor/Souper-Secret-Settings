a81ac9b8353e2be04f3785fd583915acc378225d
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-05-03 15:06:23 +0100

    set ShaderTime.currentRenderType during ParameterScreen render

--- src/main/java/com/nettakrim/souper_secret_settings/gui/parameters/ParameterScreen.java
+++ src/main/java/com/nettakrim/souper_secret_settings/gui/parameters/ParameterScreen.java
@@ -1,11 +1,15 @@
 package com.nettakrim.souper_secret_settings.gui.parameters;
 
+import com.mclegoman.luminance.client.shaders.Shader;
+import com.mclegoman.luminance.client.shaders.ShaderTime;
+import com.nettakrim.souper_secret_settings.SouperSecretSettingsClient;
 import com.nettakrim.souper_secret_settings.gui.ListScreen;
 import com.nettakrim.souper_secret_settings.gui.ListWidget;
 import com.nettakrim.souper_secret_settings.gui.SoupGui;
 import com.nettakrim.souper_secret_settings.shaders.ShaderLayer;
 import com.nettakrim.souper_secret_settings.shaders.calculations.Calculations;
 import com.nettakrim.souper_secret_settings.shaders.calculations.Calculation;
+import net.minecraft.client.gui.DrawContext;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +21,13 @@ public class ParameterScreen extends ListScreen<Calculation> {
     public ParameterScreen(int scrollIndex, ShaderLayer layer) {
         super(scrollIndex);
         this.layer = layer;
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        ShaderTime.currentRenderType = SouperSecretSettingsClient.soupRenderer.getRenderType();
+        super.render(context, mouseX, mouseY, delta);
+        ShaderTime.currentRenderType = Shader.RenderType.UI;
     }
 
     @Override
